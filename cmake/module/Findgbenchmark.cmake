bdfda601f791d89bacedb8f02266ad6ecf6e5b5c
sunyunbo <sunyunbo@megvii.com>
2021-08-27 00:34:36 +0800

    Add benchmark template

---
+++ cmake/module/Findgbenchmark.cmake
@@ -0,0 +1,20 @@
+include(FetchContent)
+
+FetchContent_Declare(
+	gbenchmark_fc
+	GIT_REPOSITORY	https://github.com/google/benchmark.git
+	GIT_TAG			2d054b683f293dbee21f8fe8820d967360e6af83 # v1.5.6
+	SOURCE_DIR		${THIRD_PARTY_SOURCE_DIR}/gbenchmark
+)
+
+if(NOT gbenchmark_fc_POPULATED)
+	FetchContent_Populate(gbenchmark_fc)
+endif()
+
+# set(BENCHMARK_DOWNLOAD_DEPENDENCIES OFF CACHE BOOL "" FORCE)
+set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "" FORCE)
+set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "" FORCE)
+
+add_subdirectory(${gbenchmark_fc_SOURCE_DIR} ${CMAKE_BINARY_DIR}/gbenchmark)
+
+add_library(gbenchmark ALIAS benchmark)
---
+++ src/sum_benchmark.cc
@@ -0,0 +1,13 @@
+#include "benchmark/benchmark.h"
+#include "sum.h"
+
+static void BM_Sum(benchmark::State &state) {
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+    sum(1, 2);
+  }
+}
+
+BENCHMARK(BM_Sum);
+
