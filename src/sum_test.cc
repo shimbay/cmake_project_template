a6ba8fdcdd9e59a6296be6ec3bbd03951b681f50
sunyunbo <sunyunbo@megvii.com>
2021-08-26 23:40:54 +0800

    Project init

---
+++ src/sum_test.cc
@@ -0,0 +1,11 @@
+#include "sum.h"
+#include "gtest/gtest.h"
+
+class SumTest : public ::testing::Test {
+public:
+  static void SetUpTestSuite() {}
+
+  static void TearDownTestSuite() {}
+};
+
+TEST_F(SumTest, Sum) { ASSERT_EQ(3, sum(1, 2)); }
