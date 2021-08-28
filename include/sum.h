6b6df1335e82a7ad38efc91bd7a5f4b9a3022a17
sunyunbo <sunyunbo@megvii.com>
2021-08-28 10:27:40 +0800

    Opt doxygen config

--- include/sum.h
+++ include/sum.h
@@ -1,3 +1,19 @@
 #pragma once
 
+/**
+ * @brief sum input
+ */
+typedef struct SumInput {
+  int a;
+  int b;
+} SumInput;
+
+/**
+ * @brief compute sum result of input a, input b
+ *
+ * @param a input a
+ * @param b input b
+ *
+ * @return sum result
+ */
 int Sum(int a, int b);
